6067e61f5d10b0dae684c0ffff3ba3efff8e1256
RootUnited <54714416+RootUnited@users.noreply.github.com>
2019-08-30 19:44:26 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<? Php
+/ *
+  Bu, SITEBILGI'ın blogcuları taramasına ilişkin moz erişim kimliği ve gizli anahtar için yapılandırma dosyasıdır.
+  Eğer bir blog yazarı değilseniz ya da başka bir deyişle, bazı geeky blogcularının hedef site hakkındaki bilgileri ile ilgilenmiyorsanız, bu dosyayı yalnız bırakabilirsiniz.
+
+  - Ama ilgileniyorum ve bu anahtarları nereden alacağımı bilmiyorum: '(...
+  - Bu durumda, moz.com'da bir hesap oluşturun ve anahtarlarınızı burada oluşturun: https://moz.com/products/mozscape/access
+
+  LÜTFEN DİKKAT: ücretsiz hesapların ayda 27.000 çağrı sınırlaması var, bu benim için yeterli olduğunu düşünüyorum ancak bu sizin için yetersiz kalırsa, sadece moz içinde yeni bir hesap oluşturun ve yeni anahtarlar alın ve eski hesaplarınızı değiştirin.
+* /
+
+$ accessID = "erişim kimliğinizi tırnak işaretleri arasına koyun"; // erişim kimliğinizi girdiğiniz yer
+$ secretKey = "gizli anahtarınızı tırnak işaretleri arasına yapıştırın"; // Gizli anahtarın buraya gidiyor
+
+?>
